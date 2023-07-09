f348239f8a1ba70afd030524b6b5896724f8264a
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-07-09 14:52:38 +0300

    MenuUI sounds hover

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,24 +1,57 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MainMenuUI : MonoBehaviour
+public class MainMenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField]
-    private Button startButton;
+  [SerializeField]
+  private Button startButton;
 
-    [SerializeField]
-    private Button exitButton;
+  [SerializeField]
+  private Button exitButton;
+  [SerializeField]
+  private AudioSource hoverSound;
+  [SerializeField]
+  private AudioSource clickSound;
+  [SerializeField]
+  private Color hoverColor;
+  [SerializeField]
+  private Color normalColor;
 
-    private void Awake()
+  private void Awake()
+  {
+    startButton.onClick.AddListener(() =>
     {
-        startButton.onClick.AddListener(() =>
-        {
-            Loader.LoadScene(Loader.ScenesEnum.GameScene);
-        });
-        exitButton.onClick.AddListener(() =>
-        {
-            Application.Quit();
-        });
-    }
+      PlayClickSound();
+      Loader.LoadScene(Loader.ScenesEnum.GameScene);
+    });
+    exitButton.onClick.AddListener(() =>
+    {
+      PlayClickSound();
+      Application.Quit();
+    });
+  }
+
+  void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+  {
+    PlayHoverSound();
+    startButton.image.color = hoverColor;
+  }
+
+  void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+  {
+    PlayHoverSound();
+    startButton.image.color = normalColor;
+  }
+
+  private void PlayHoverSound()
+  {
+    hoverSound.Play();
+  }
+
+  private void PlayClickSound()
+  {
+    clickSound.Play();
+  }
 }
